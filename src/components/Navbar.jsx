24193258395f0984bc518b1611d9fb15ffc57c74
kalleolumets <olumetskalle@gmail.com>
2023-11-04 09:24:18 +0200

    name change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
       id="top"
       className="relative w-full sm:flex justify-between items-center p-2"
     >
-      <h1 className="font-bold text-gray-300">Dashboard</h1>
+      <h1 className="font-bold text-gray-300">Dynamic NFTs </h1>
       <div className="py-2">
         <TextInput icon={SearchIcon} placeholder="Search..." />
       </div>
